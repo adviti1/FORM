c3848bc0090d6d49a41e279d57a927d75d8da364
adviti1 <113535364+adviti1@users.noreply.github.com>
2022-10-27 22:36:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 9999;
+
+
+const app = express();
+
+
+app.use(express.json());
+
+app.use(bodyParser.json());
+
+app.use(express.urlencoded());
+
+app.use(express.static('public'));
+
+
+
+
+app.get('/form', (req, res) => {
+
+    res.sendFile(_dirname + '/public/index.html');
+
+})
+
+app.post('/formPost', (req, res) => {
+    console.log(req.body);
+
+})
+
+
+
+
+app.listen(9999, () => {
+    console.log('server started at http://localhost:${port}')
+})
